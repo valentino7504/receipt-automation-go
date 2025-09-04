4d14cd2d1fd173146caa41c81057e6e3a4c077a4
Edwin Ade <edwinmbonyjr@gmail.com>
2025-09-04 23:59:02 +0100

    feat(auth): return token in Login

--- internal/auth/login.go
+++ internal/auth/login.go
@@ -3,19 +3,31 @@ package auth
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
-func Login(ctx context.Context) error {
+func Login() (*UserToken, error) {
+	// Request device code from Microsoft device code endpoint
 	deviceCode, err := requestDeviceCode()
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	// Create context and get the user to authenticate
+	expiryTime := time.Now().Add(time.Duration(deviceCode.ExpiresIn) * time.Second)
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		time.Duration(deviceCode.ExpiresIn)*time.Second,
+	)
+	defer cancel()
 	fmt.Printf("Device code: %s\n", deviceCode.UserCode)
+	fmt.Printf("Expires by %v\n", expiryTime.Format("3:04pm"))
 	fmt.Printf("URL: %s\n", deviceCode.VerificationURI)
+
+	// Gets Access Token
 	userToken, err := getAccessToken(ctx, deviceCode)
 	if err != nil {
-		return nil
+		return nil, err
 	}
-	fmt.Println(*userToken)
-	return nil
+	return userToken, nil
 }
